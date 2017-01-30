43e49e844dd460eeeb7f60146658bd11e341a261
Nastya Hurska <nastyagurska@gmail.com>
2017-01-30 00:42:58 -0200

    add slider and tabs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){ 
     
     
-    
+    // animation on page
     $(".part").animated("fadeInDown", "pulse");
 
     $("#btn-toogle").click(function() {
@@ -16,5 +16,26 @@ $(document).ready(function(){
         }, 800);
         return false;
         });  
+    
+    //slider
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 1,
+        loop: true
+	});
+	owl.on(".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});
     
     });
